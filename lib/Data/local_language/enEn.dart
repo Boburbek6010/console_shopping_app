affce93b020324fbc2007e8a45c03d81a20a3750
bekpulatovkamol <bekpulatovkamol54@gmail.com>
2023-12-03 00:53:09 +0500

    Kamol, 3-commit, adding values in Maps of en,ru,uz

--- lib/Data/local_language/enEn.dart
+++ lib/Data/local_language/enEn.dart
@@ -3,5 +3,10 @@ const Map<String, String> en = {
   "sign_up":"1. Sign Up",
   "sign_in":"2. Sign In",
   "exit":"3. Exit",
+  "language_choice":"Enter your choice: ",
+  "exit_text":"Thank you for using our app...",
+  "invalid_value_text":"Invalid choice. Please try again.",
+  "email_enter":"Enter your email: ",
+  "email_invalid":"Invalid email format. Please enter a valid email address.",
 
 };
--- lib/Data/local_language/uzUz.dart
+++ lib/Data/local_language/uzUz.dart
@@ -3,5 +3,10 @@ const Map<String, String> uz = {
   "sign_up":"1. Registratsiya",
   "sign_in":"2. Kirish",
   "exit":"3. Chiqish",
+  "language_choice":"Tanlovingizni kiriting: ",
+  "exit_text":"Ilovamizdan foydalanganingiz uchun rahmat...",
+  "invalid_value_text":"Not`og'ri tanlov. Iltimos yana bir marta urunib kuring.",
+  "email_enter":"Email kiriting: ",
+  "email_invalid":"Elektron pochta formati noto‘g‘ri. Iltimos, to'g'ri e-pochta manzilini kiriting.",
 
 };
