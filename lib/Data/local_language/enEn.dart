89b6c5e8394086f644a0ea3d2789ed2c73239f64
bekpulatovkamol <bekpulatovkamol54@gmail.com>
2023-12-03 16:27:55 +0500

    Kamol, 4-commit, adding values in Maps of en,ru,uz

--- lib/Data/local_language/enEn.dart
+++ lib/Data/local_language/enEn.dart
@@ -1,16 +1,35 @@
 const Map<String, String> en = {
-  "welcome":"Welcome to our app",
-  "setting":"Setting menu",
-  "exit":"Exit",
-  "admin" : "Administrator",
-  "user" : "user",
-  "Change_lang" : "Change Language",
   "home_menu":"Home Menu",
-  "sign_up":"Sign Up",
-  "sign_in":"Sign In",
-  "language_choice":"Enter your choice",
+  "sign_up":"1. Sign Up",
+  "sign_in":"2. Sign In",
+  "exit":"3. Exit",
+  "language_choice":"Enter your choice: ",
   "exit_text":"Thank you for using our app...",
   "invalid_value_text":"Invalid choice. Please try again.",
-  "email_enter":"Enter your email",
-  "email_invalid":"Invalid email format. Please enter a valid email address"
+  "email_enter":"Enter your email: ",
+  "email_invalid":"Invalid email format. Please enter a valid email address.",
+  "login":"login = ",
+  "password":"password = ",
+  "error":"Error",
+  "smth_went_wrong":"Something went wrong at ",
+  "successfully_posted":"Successfully posted: ",
+  "already_have_email":"Email is already registered. Please use a different email.",
+  "check_password":""""
+      Password requirements:
+      - Have more than 8 characters.
+      - Contains a capital letter.
+      - Contains a lowercase letter.
+      - Contains a number.
+    """"",
+  "enter_password":"Enter your password: ",
+  "password_invalid":"Invalid password format. Please make sure it meets the requirements.",
+  "enter_name":"Enter your name: ",
+  "invalid_name":"Invalid name format. Please enter a valid name.",
+  "enter_surname":"Enter your surname: ",
+  "enter_age":"Enter your age: ",
+  "invalid_age":"Invalid age. Please enter a valid age.",
+  "enter_phone":"Enter your phone number: ",
+  "successfully_registered":"Successfully registered!",
+  "welcome":"Welcome, ",
+
 };
